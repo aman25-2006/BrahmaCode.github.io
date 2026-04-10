4b87d1dd9f44c143b7c2816060723d233d359b5b
Aman Kumar <asiaaman988@gmail.com>
2026-04-10 11:08:43 +0530

    Remove exposed Firebase config values

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // Paste your Firebase web app config here when you create a Firebase project.
 // Leave these empty until then; the admin system will fall back to localStorage.
 window.BRAHMACODE_FIREBASE_CONFIG = {
-  apiKey: 'AIzaSyA3sQBeLJy4t-RJjROPN7RQOPDUG7O67iw',
-  authDomain: 'brahmacode-d9926.firebaseapp.com',
-  projectId: 'brahmacode-d9926',
-  storageBucket: 'brahmacode-d9926.firebasestorage.app',
-  messagingSenderId: '596064695265',
-  appId: '1:596064695265:web:3f40b61208fec0be2658a0',
-  measurementId: 'G-P3VRPYTYBY'
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: ''
 };
